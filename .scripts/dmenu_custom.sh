6244a352c05a69eadf28b27c41ef0ddcc57c0d49
IgLemp <iglemp@protonmail.com>
2024-11-04 16:11:21 +0100

    something

--- .scripts/dmenu_custom.sh
+++ .scripts/dmenu_custom.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-PICK=$(echo -e "mat\nfiz\nplan" | dmenu)
- 
+# PICK=$(echo -e "mat\nfiz\nplan\ncalc" | dmenu)
+PICK=$(echo -e "plan\nmat\nfiz\ncalc\nck\nmlap\nlap" | dmenu) 
+
 case $PICK in
 	"mat")
 		evince ~/Documents/wybrane_wzory_matematyczne_EM2023.pdf
 		;;
 	"fiz")
-		evince ~/Documents/wybrane_wzory_stale_fizykochemiczne_EM2023.pdf
+		evince ~/Documents/liceum/wybrane_wzory_stale_fizykochemiczne_EM2023.pdf
+		;;
+	"mlap")
+		evince ~/Documents/Transformata\ Laplacea\ -\ tabela\ wzorów.pdf
+	  ;;
+	"lap")
+	  evince ~/Documents/LaplaceTablica.pdf
 		;;
 	"plan")
-		evince ~/Documents/plan.pdf
+		evince ~/Documents/plan_studiów.pdf
 		;;
+	"calc")
+		gnome-calculator
+		;;
+	"ck")
+		evince ~/Documents/wzorynacalki.pdf
 esac
